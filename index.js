e1636bbafe4b30b674ef8782234f5812c400af0b
FrostXk9 <134713740+FrostXk9@users.noreply.github.com>
2023-07-23 17:59:39 +0200

    Update index.js
    injected my js code for submit form

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 // Js code for a form element with the id "login-form" in your HTML
 
-// Getting the form element by its id in the html document
-const form = document.getElementById('login-form');
+// Get the button element by its id
+const submit = document.getElementById("submitLogIn");
 
-// Adds an event listener to the form's submit event button
-form.addEventListener('submit', function(event) {
+// Adds an event listener to the form's submit event
+submit.addEventListener('click', function(event) {
   event.preventDefault(); // Prevent the default form submission
 
   // Gets the values from the form inputs
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+  //input is declared for password and email
+  let input = document.getElementById("input");
 
-  // Perform any necessary validation or processing with the username and password here
+// Perform any necessary validation or processing with the username and password here
+  const forEmailAndPassword = input.value.trim();
+  if(forEmailAndPassword ===''){
+    alert('please enter valid information');
+  }else{window.location.href = 'index.html';}// Redirects the user to the new page
 
-  // Redirects the user to the new page
-  window.location.href = './home.html';
+  // const email = input.value.trim();
+  // const UserPassword = input.value.trim();
+  // if(email ==='' || UserPassword ===''){
+  //   alert('Insert valid details please');
+  // }
 });
-
 
  
  
